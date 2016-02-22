57b90742df51c2fead176564bdfd545b9587b7d4
fetko1977 <feddfriend77@gmail.com>
2016-02-22 23:29:18 +0200

    commit changes

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'YybIkFE2xTV4w3JAZYnRNLWELhA1nLglmlxqH9oh',
+  masterKey: process.env.MASTER_KEY || 'ESxjcxO3N5UDJrxYbWECL1Y3M9c89U4Fxq3fZsmg', //Add your master key here. Keep it secret!
+  restAPIKey: process.env.REST_API_KEY || 'gqvb52ROdHhcav4lAq7vx0qvuYQNXROWOb7u3nLB'
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
